32e29e015b8402cd1e22bb838f5edb5cee208b64
Kossti <timoshenko.konstantin@gmail.com>
2023-04-26 20:14:18 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import user from "./Profile/user.json"
+import Profile from "./Profile/Profile";
+
+
 export const App = () => {
   return (
     <div
@@ -7,9 +11,18 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        margin: 'auto',
+        padding: 20
       }}
     >
+      <Profile
+      avatar={user.avatar}
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      stats={user.stats}
+    />
       React homework template
     </div>
   );
